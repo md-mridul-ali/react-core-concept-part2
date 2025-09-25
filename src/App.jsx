1a7c60641c9b96b85417ff14bf8e16923b51a90d
md-mridul-ali <mridulaiub17@gmail.com>
2025-09-26 03:47:42 +0600

    add some new topic

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,27 @@
 // import viteLogo from '/vite.svg'
 import Counter from './counter'
 import Batsman from'./batsman'
+import Users from './users'
 import './App.css'
+import { Suspense } from 'react'
+import Friends from './friends'
+
+
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json())
+
+
+const fetchFriends = async() =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    return res.json();//promise ke return korbe
+}
+
 
 function App() {
   // const [count, setCount] = useState(0)
+
+   const friendsPromise = fetchFriends();
+
 
   function handleClick() {
     alert('I am clicked')
@@ -26,6 +43,14 @@ function App() {
     <>
 
       <h3>Vite + React</h3>
+
+      {/* <Suspense fallback={<h3>Loading...</h3>}>
+        <Users fetchUsers={fetchUsers}></Users>
+      </Suspense> */}
+
+      <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
+           <Friends  friendsPromise={friendsPromise}></Friends>
+      </Suspense>
 
       <Batsman></Batsman>
 
